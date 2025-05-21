1e79c3648070c60c8dedeb1cbba9a39f3eb5269d
Lucas <abad.lucas.b@gmail.com>
2025-05-21 21:45:21 +0200

    Solved some Leetcode Exercises

---
+++ Arrays & Hashing/ValidAnagram.py
@@ -0,0 +1,32 @@
+"""
+Valid Anagram
+Solved 
+Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+
+An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+Example 1:
+
+Input: s = "racecar", t = "carrace"
+
+Output: true
+Example 2:
+
+Input: s = "jar", t = "jam"
+
+Output: false
+Constraints:
+
+s and t consist of lowercase English letters.
+"""
+
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        
+        cnt_s = Counter(s)
+        cnt_t = Counter(t)
+
+        return cnt_s == cnt_t
